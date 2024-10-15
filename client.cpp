472ddd287833072b030741c3404f9fb52f0e16c4
HeewoongChrisCha <hcha@cpp.edu>
2024-10-15 10:59:13 +0900

    [Merge] 박민기
    Clientsocket Main Merge

--- client.cpp
+++ client.cpp
@@ -1,10 +1,10 @@
 #include "client.h"
 #include <QHostAddress>
-#include <QDebug>
 #include <QString>
-#include <Qvector>
+#include <QDebug>
 
-Client::Client(QObject *parent) : QObject(parent), socket(new QTcpSocket(this))
+Client::Client(QObject *parent)
+    : QObject(parent), socket(new QTcpSocket(this))
 {
     // 소켓과 관련된 시그널과 슬롯 연결
     connect(socket, &QTcpSocket::connected, this, &Client::onConnected);
@@ -22,64 +22,228 @@ Client::~Client()
         delete socket;
     }
 }
-QStringList Client::getpostList()
-{
-    return postList;
-}
 
 void Client::connectToServer(const QString &host, quint16 port)
 {
     // 서버 IP와 포트로 연결 시도
     socket->connectToHost(QHostAddress(host), port);
-}
-
-void Client::sendMessage(const QString &message)
-{
-    socket->write(message.toUtf8());
+    setFlag(3);
+    sendMessage("3:1");
 }
 
 void Client::onConnected()
 {
     // 서버에 연결되었을 때 호출됨
     qDebug() << "Connected to server!";
-    this->initPost();
 }
 
-void Client::initPost()
+void Client::setFlag(int num)
 {
-    this->sendMessage("3:1");
-    QByteArray data = socket->readAll();
-
-    QString dataString = QString::fromUtf8(data);
-    postList = dataString.split("\n");
-
-    qDebug() << dataString << "\n";
-    for (int i=0;i<postList.size();i++) {
-        qDebug() << postList[i] << "\n";
-    }
-
+    flag = num;
 }
 
-// void Client::cmpMember()
+
+void Client::sendMessage(const QString &message)
+{
+    socket->write(message.toUtf8());
+}
+
+
+void Client::sendLogin(QString id, QString pw)
+{
+    QString result = QString("%1:%2:%3").arg("1").arg(id).arg(pw);
+    setFlag(1);
+    sendMessage(result);
+}
+
+void Client::addMembership(QString id, QString nickName, QString pw)
+{
+    QString result = QString("%1:%2:%3").arg("2").arg(id).arg(pw);
+    setFlag(2);
+    sendMessage(result);
+}
+
+void Client::postGet(QString postNum)
+{
+    QString result = QString("%1:%2").arg("3").arg(postNum);
+    setFlag(4);
+    sendMessage(result);
+}
+
+void Client::writePost(QString title, QString nickname, QString detail)
+{
+    QString result = QString("%1:%2:%3").arg("5").arg(nickname).arg(detail);
+    setFlag(5);
+    sendMessage(result);
+}
+
+// void Client::modifyPost(Post post)
 // {
-//     socket->write("1");
-//     QByteArray data = socket->readAll();
-//     QString dataString = QString::fromUtf8(data);
-//     QStringList LoginList = dataString.split("\n");
-//     for(int i = 0; i < LogList; i+=2)
-//     {
-//         if(LogintList[i]=="id" && LoginList[i+1]=="pw")
-//         {
-//             id = LoginList[i];
-//             pw = LoginList[i+1];
-//         }
-//     }
+//     QString result = QString("%1:%2:%3").arg("6").arg().arg(detail);
+//     setFlag(6);
+//     sendMessage(result);
 // }
+
+// void Client::deletePost(Post post)
+// {
+//     QString result = QString("%1:%2:%3").arg("7").arg(nickname).arg(detail);
+//     setFlag(7);
+//     sendMessage(result);
+// }
+
+// void Client::writecomment(Post post, Comment comment.txt)
+// {
+//     QString result = QString("%1:%2:%3").arg("8").arg(nickname).arg(detail);
+//     setFlag(8);
+//     sendMessage(result);
+// }
+
+// void Client::modifycomment(Post post, Comment comment.idx, Comment comment.txt)
+// {
+//     QString result = QString("%1:%2:%3").arg("9").arg(nickname).arg(detail);
+//     setFlag(9);
+//     sendMessage(result);
+// }
+
+// void Client::deleteComment(Post post, Comment comment.idx)
+// {
+//     QString result = QString("%1:%2:%3").arg("10").arg(nickname).arg(detail);
+//     setFlag(10);
+//     sendMessage(result);
+// }
+
+// void Client::outMembership(Info info)
+// {
+//     QString result = QString("%1:%2:%3").arg("11").arg(info.MemberId).arg(info.MemberPw);
+//     setFlag(11);
+//     sendMessage(result);
+// }
+
+
+
+
 
 void Client::onReadyRead()
 {
     // 서버로부터 데이터가 수신되었을 때 호출됨
-    qDebug() << "Received from server:";
+    QByteArray data = socket->readAll();
+    QString datas = QString::fromUtf8(data);
+    QStringList parsedData = datas.split('\n'); // QString datats[]
+    bool ok = true;
+    switch (flag) {
+    case 1: // tryLogin
+        if (parsedData[0] == "-1") {
+            qDebug() << "ID Wrong!";
+            // IDWRONG SIG
+        } else if (parsedData[0] == "-2") {
+            qDebug() << "PW Wrong!";
+            // PWWRONG SIG
+        } else {
+            qDebug() << "login success!";
+            cliInfo.MemberId = parsedData[0];
+            cliInfo.MemberPw = parsedData[1];
+            cliInfo.MemberName = parsedData[2];
+            cliInfo.admin = parsedData[3].toInt(&ok);
+        }
+        break;
+
+    case 2: // setMembership
+        if (parsedData[0] == "-1") {
+            qDebug() << "ID Overlap";
+            // 닉네임 중복
+        } else if (parsedData[0] == "-2") {
+            qDebug() << "your our member!!";
+            // 닉네임 중복
+        } else {
+            qDebug() << "membership error";
+        }
+        break;
+
+    case 3: // allpostGet
+        if (parsedData[0] == "0") {
+            qDebug() << "AllpostGet error";
+        } else {
+            postTable = std::move(parsedData);
+            for(int i = 0; i < postTable.size(); i++)
+            {
+                qDebug() << postTable[i];
+            }
+        }
+        break;
+
+    case 4: // postGet
+        if (parsedData[0] == "0") {
+            qDebug() << "postGet error";
+        } else {
+            post = parsedData[0];
+        }
+        break;
+
+    // case 5: // WritePost
+    //     if (parsedData[0] == "0") {
+    //         qDebug() << "WritePost error";
+    //     } else if (parsedData[0] == "1") {
+    //         bool ok = true;
+    //         int id = parsedData[0].toInt(&ok); // ??? id 어디에 쓸지??
+    //         // 추가 처리가 필요할 수 있습니다.
+    //     }
+    //     break;
+
+    // case 6: // modifyPost
+    //     if (parsedData[0] == "0") {
+    //         qDebug() << "modifyPost error";
+    //     } else if (parsedData[0] == "1") {
+    //         qDebug() << "modifyPost success";
+    //     }
+    //     break;
+
+    // case 7: // deletePost
+    //     if (parsedData[0] == "0") {
+    //         qDebug() << "deletePost error";
+    //     } else if (parsedData[0] == "1") {
+    //         qDebug() << "deletePost success";
+    //     }
+    //     break;
+
+    // case 8: // Writecomment
+    //     if (parsedData[0] == "0") {
+    //         qDebug() << "error";
+    //     } else if (parsedData[0] == "1") {
+    //         qDebug() << "success";
+    //     }
+    //     break;
+
+    // case 9: // modifycomment
+    //     if (parsedData[0] == "0") {
+    //         qDebug() << "error";
+    //     } else if (parsedData[0] == "1") {
+    //         qDebug() << "success";
+    //     }
+    //     break;
+
+    // case 10: // deleteComment
+    //     if (parsedData[0] == "0") {
+    //         qDebug() << "error";
+    //     } else if (parsedData[0] == "1") {
+    //         qDebug() << "success";
+    //     }
+    //     break;
+
+    // case 11: // outMembership
+    //     if (parsedData[0] == "-1") {
+    //         qDebug() << "Id Wrong";
+    //     } else if (parsedData[0] == "-2") {
+    //         qDebug() << "Pw Wrong";
+    //     } else if (parsedData[0] == "0") {
+    //         qDebug() << "success_out";
+    //     }
+    //     break;
+
+    default:
+        qDebug() << "Unknown flag value!";
+        break;
+    }
+
 }
 
 void Client::onDisconnected()
--- client.h
+++ client.h
@@ -3,19 +3,37 @@
 
 #include <QObject>
 #include <QTcpSocket>
+#include <QString>
 
 class Client : public QObject
 {
     Q_OBJECT
+
 public:
+    struct Info {
+        QString MemberId;
+        QString MemberPw;
+        QString MemberName;
+        int admin;
+    };
+
+
     explicit Client(QObject *parent = nullptr);
     ~Client();
 
     void connectToServer(const QString &host, quint16 port); // 서버에 연결
-    void sendMessage(const QString &message); // 서버로 메시지 전송
-    void initPost();
-    void cmpMember();
-    void joinMember();
+    void sendMessage(const QString &message);
+    void setFlag(int num);
+    void sendLogin(QString id, QString pw);
+    void addMembership(QString id, QString nickName, QString pw);
+    void postGet(QString postNum);
+    void writePost(QString title, QString nickname, QString detail);
+    // void modifyPost(Post post)
+    //     void deletePost(Post post)
+    //     void writecomment(Post post, Comment comment.txt)
+    //     void modifycomment(Post post, Comment comment.idx, Comment comment.txt)
+    //     void deleteComment(Post post, Comment comment.idx)
+    //     void subMembership(Info info)
 
 private slots:
     void onConnected();      // 서버에 연결되었을 때 호출
@@ -24,14 +42,12 @@ private slots:
     void onErrorOccurred(QAbstractSocket::SocketError socketError); // 에러 발생 시 호출
 
 private:
+
+    int flag;
+    Info cliInfo;
+    QStringList postTable;
+    QString post;
     QTcpSocket *socket;  // TCP 소켓 객체
-    QStringList postList;
-    QString id;
-    int pw;
-public:
-    QStringList getpostList();
-
-
 };
 
 #endif // CLIENT_H
